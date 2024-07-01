7329bd46bb152a283f45fb75c70d1a0d3e85ad93
zero <daohu527@gmail.com>
2024-07-01 11:35:29 +0000

    fix: delete osqp test

--- math/math_utils_test.cc
+++ math/math_utils_test.cc
@@ -18,7 +18,6 @@
 
 #include "gtest/gtest.h"
 
-#include "include/osqp.h"
 
 namespace mimas {
 namespace math {
@@ -119,93 +118,6 @@ TEST(MathUtilsTest, AlmostEqualTest) {
   double y = Square(x_root);
   EXPECT_TRUE(almost_equal(x, y, 2));
   EXPECT_FALSE(almost_equal(y, 2.0001, 2));
-}
-
-TEST(MathUtilsTest, QPSTTest) {
-  /* Load problem data */
-  OSQPFloat P_x[3] = {
-      4.0,
-      1.0,
-      2.0,
-  };
-  OSQPInt P_nnz = 3;
-  OSQPInt P_i[3] = {
-      0,
-      0,
-      1,
-  };
-  OSQPInt P_p[3] = {
-      0,
-      1,
-      3,
-  };
-  OSQPFloat q[2] = {
-      1.0,
-      1.0,
-  };
-  OSQPFloat A_x[4] = {
-      1.0,
-      1.0,
-      1.0,
-      1.0,
-  };
-  OSQPInt A_nnz = 4;
-  OSQPInt A_i[4] = {
-      0,
-      1,
-      0,
-      2,
-  };
-  OSQPInt A_p[3] = {
-      0,
-      2,
-      4,
-  };
-  OSQPFloat l[3] = {
-      1.0,
-      0.0,
-      0.0,
-  };
-  OSQPFloat u[3] = {
-      1.0,
-      0.7,
-      0.7,
-  };
-  OSQPInt n = 2;
-  OSQPInt m = 3;
-
-  /* Exitflag */
-  OSQPInt exitflag = 0;
-
-  // Problem settings
-  OSQPSolver *solver;
-  OSQPSettings *settings;
-  OSQPCscMatrix *P = malloc(sizeof(OSQPCscMatrix));
-  OSQPCscMatrix *A = malloc(sizeof(OSQPCscMatrix));
-
-  /* Populate matrices */
-  csc_set_data(A, m, n, A_nnz, A_x, A_i, A_p);
-  csc_set_data(P, n, n, P_nnz, P_x, P_i, P_p);
-
-  settings = reinterpret_cast<OSQPSettings *>(c_malloc(sizeof(OSQPSettings)));
-  if (settings) {
-    osqp_set_default_settings(settings);
-    settings->alpha = 1.0;
-  }
-
-  /* Setup solver */
-  exitflag = osqp_setup(&solver, P, q, A, l, u, m, n, settings);
-
-  /* Solve problem */
-  if (!exitflag) {
-    exitflag = osqp_solve(solver);
-  }
-
-  /* Cleanup */
-  osqp_cleanup(solver);
-  if (A) free(A);
-  if (P) free(P);
-  if (settings) free(settings);
 }
 
 }  // namespace math
